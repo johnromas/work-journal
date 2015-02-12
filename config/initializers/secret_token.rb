2052a1a44799e315c3f15d8b806501bbb1d2ccdc
John Romas <johnr@procore.com>
2015-02-12 03:06:01 +0000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WorkJournal::Application.config.secret_key_base = 'c0cc71590abaae08f6237ee2eb200179d39387f97dd62e3b81142c242fcb52ce58b3d34ccbccbc950120bfb85227b05950a708089d6780f027653ca35e5bf3f3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WorkJournal::Application.config.session_store :cookie_store, key: '_work-journal_session'
